e4db9b690d2349953b3c6803d115da7914592e7f
johnam24 <mcgovernj@etown.edu>
2025-10-02 20:39:09 -0400

    Working on main menu; typewriter effect seems to be working

--- game/menu.java
+++ game/menu.java
@@ -4,41 +4,48 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class menu {
-    private final String textToType;
-    private final Timer timer;
-    private int characterIndex = 0;
-    private final JTextArea terminal;
 
+    private final JTextArea terminal = new JTextArea();
+    private Timer timer;
 
+    private int characterIndex;
+    private int textDelay;
 
+    public menu() {
 
-    public menu(JTextArea terminal, String text) {
-        this.textToType = text;
+        textDelay = 50;
 
+    }
 
-        this.timer = new Timer(50, new ActionListener() {
-            @Override
+    private void writeText(JTextArea terminal, String text) {
+        
+        characterIndex = 0;
+        this.timer = new Timer(textDelay, new ActionListener() {
+            
             public void actionPerformed(ActionEvent e) {
-                if (characterIndex < textToType.length()) {
-                    terminal.append(String.valueOf(textToType.charAt(characterIndex)));
+
+                if (characterIndex < text.length()) {
+                    
+                    terminal.append(String.valueOf(text.charAt(characterIndex)));
                     characterIndex++;
-                } else {
-                    timer.stop();
-                }
+
+                } else timer.stop();
+
             }
+
         });
-        this.terminal = new JTextArea();
-    };
-    public void start() {
+
         timer.start();
+
     }
     
     public static void main(String[] args) {
+
+        menu main = new menu();
+
         JFrame frame = new JFrame("Silver Slayer RPG");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 1000);
-
-
 
         // Display
         JTextArea terminal = new JTextArea();
@@ -51,21 +58,22 @@ public class menu {
         terminal.setFont(new Font("Cascadia Mono", Font.PLAIN, 20));
         
         JScrollPane scrollPane = new JScrollPane(terminal);
-    
 
         // Input
         JTextField inputField = new JTextField();
         inputField.addActionListener((ActionEvent e) -> {
-            String command = inputField.getText();
-            terminal.append("> " + command + "\n\n");
+
+            main.writeText(terminal, inputField.getText());
             inputField.setText("");
+
         });
 
         // Layout
         frame.setLayout(new BorderLayout());
         frame.add(scrollPane, BorderLayout.CENTER);
         frame.add(inputField, BorderLayout.SOUTH);
-
         frame.setVisible(true);
+
     }
+
 }
