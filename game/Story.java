6c6788a03a4f62d9eb883ab5d9dc44a20f0ef6ff
Martin Ratchford <ratchfordm@etown.edu>
2025-10-21 21:08:25 -0400

    Storys for all the locations. Can change if need be

--- game/Story.java
+++ game/Story.java
@@ -32,23 +32,37 @@ public class Story {
 
         // START
         start.put(100, "You step up to the gate. Any lock has long since worn away, and you're able to push the gate open with little resistance. The doors swing wide with a long, metallic screech, and you step through onto the path that leads to the village.");
-        start.put(110, "epic story event");
 
         // VILLAGE
-        village.put(200, "village moment");
-        
+        village.put(200, "You arrive at the village. Buildings worn from years of neglect and misuse. You don't see any signs of life around. At first glance, it seems like you are alone.");
+        village.put(210, "A particular house catches your eye. It seems less worn then the rest of the buildings, so you decide to take a closer look. You go inside.");
+        village.put(220, "The village graveyard is vast, with many gravestones in perfect rows. You decide to explore.");
+        village.put(230, "In the center of the village there is a well. Must have been the water source for everyone. You decide to take a look inside.");
+
         // LAKE
-        lake.put(300, "literally just a lake");
+        lake.put(300, "You voyage to a great lake. The water is completely still, but yet you can see strange looking creatures moving under the surface");
+        lake.put(310, "You have stumbled upon a dock. This would be a good place to fish, but for what?");
 
         // MOUNTAIN
+        mountain.put(400, "You arrive the base of a mountain. A long, steep path looks like it makes it way to the peak, but you cant see the top from here.");
 
         // DESERT
+        desert.put(500, "You travel to the desert. The heat is almost unbearable. In front of you are dunes that go on for what seems like forever.");
+        desert.put(510, "You walk to one of the many large dunes. You decide to look around.");
+        desert.put(520, "Beside one of the dunes, you notice a town, and decide to check it out.");
+        desert.put(530, "A small well is not far from you. Though looking inside, you see it is completely dry.");
 
         // SWAMP
+        swamp.put(600, "You make your way to a swamp. The air is humid. Your feet sink into the soft mud as you walk. Erie sounds come from within the swamp, but you cant tell what is making the noise.");
 
         // FRACTURE
+        fracture.put(700, "You have entered the fracture. The landscape doesn't look real. The ground is cracked and broken. Looking into these cracks in the ground, you see strange shapes that can only be described as supernatural. The sky seems to be constantly changing color.");
+        fracture.put(710, "You notice the lights in the sky are leading somewhere, somewhere dark, even evil. You decide to follow them.");
+        fracture.put(720, "Big secret");
 
         // LAIR
+        lair.put(800, "Crossing a rickety old bridge, you arrive at the lair. The air is thick with smoke. You only can describe this place as the underworld. Nothing good happens here. Across the bridge you see an onmious building, like a castle, but evil. You decide to go inside.");
+        lair.put(810, "You see a door that seems to enter some kind of throne room. Loud roars are coming from inside. Whatever is in there, knows your here, and plans to change that very soon. Holding your breath, you open the door and step inside.");
 
     }
 
